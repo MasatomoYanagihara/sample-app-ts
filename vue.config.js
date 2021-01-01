2a2a2e35632cd13c2c0199947b71986dc41eea51
Yana <masatomo.yanagihara.jp@gmail.com>
2021-01-01 18:05:41 +0900

    step13まで

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const path = require('path');
+const apiMocker = require('mocker-api');
 
 module.exports = {
+  devServer: {
+    port: 8888, // 8080だとエラーなる
+    before(app) {
+      apiMocker(app, path.resolve('@/../mock/api-mock.js'), {
+        proxy: {
+          '/api/(.+)': 'http://loclahost:8888/',
+        },
+        changeHost: true,
+      });
+    },
+  },
   transpileDependencies: ['vuetify'],
   configureWebpack: {
     plugins: [
