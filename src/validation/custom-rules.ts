0a4e15a5d39c1a470b032f99314e1d26f9d61317
Yana <masatomo.yanagihara.jp@gmail.com>
2021-01-02 13:49:33 +0900

    14.2 バリデーションカスタムルールの別ファイル化

---
+++ src/validation/custom-rules.ts
@@ -0,0 +1,10 @@
+import { ValidationRules } from 'vee-validate/dist/types/types';
+
+export const customRules: { [key: string]: ValidationRules } = {
+  userNameAllowedCharacters: {
+    message: '{_field_}は英字、数字、「_」のみ使用できます。',
+    validate: value => {
+      return /^[0-9A-Z_]*$/i.test(value);
+    },
+  },
+};
